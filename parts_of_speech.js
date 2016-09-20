f4de9d151cae7e09b56ae7938dad5a3875b6ab55
LosBunkos <nadavvadan@gmail.com>
2016-09-20 10:15:57 +0300

    add plural pronouns

--- parts_of_speech.js
+++ parts_of_speech.js
@@ -11,7 +11,15 @@ class PronounGroup {
 }
 const singular_pronouns = [
   new PronounGroup("I", "me", "my", "myself"),
+  new PronounGroup("you", "you", "your", "yourself"),
+  new PronounGroup("it", "it", "its", "itself"),
   new PronounGroup("he", "him", "his", "himself"),
   new PronounGroup("she", "her", "hers", "herself"),
-  new PronounGroup("they", "them", "their", "themselves")
+  new PronounGroup("they", "them", "their", "themselves"),
   ]
+
+const plural_pronouns = [
+  new PronounGroup("we", "us", "ours", "ourselves"),
+  new PronounGroup("they", "them", "their", "themselves"),
+  new PronounGroup("you", "you", "your", "yourselves")
+]
