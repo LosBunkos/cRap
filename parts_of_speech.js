7cab00409085e4e836485a28f187b1c465ffdfa3
LosBunkos <nadavvadan@gmail.com>
2016-09-20 10:17:25 +0300

    tiny cleanup

--- parts_of_speech.js
+++ parts_of_speech.js
@@ -9,6 +9,7 @@ class PronounGroup {
     this.myself = myself;
   }
 }
+
 const singular_pronouns = [
   new PronounGroup("I", "me", "my", "myself"),
   new PronounGroup("you", "you", "your", "yourself"),
@@ -16,7 +17,7 @@ const singular_pronouns = [
   new PronounGroup("he", "him", "his", "himself"),
   new PronounGroup("she", "her", "hers", "herself"),
   new PronounGroup("they", "them", "their", "themselves"),
-  ]
+]
 
 const plural_pronouns = [
   new PronounGroup("we", "us", "ours", "ourselves"),
