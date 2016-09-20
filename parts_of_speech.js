7cdba51b2ea0de2d96897842a5c640990157ea5a
LosBunkos <nadavvadan@gmail.com>
2016-09-20 10:00:29 +0300

    add PronounGroup class and singular pronouns (nadav)

--- parts_of_speech.js
+++ parts_of_speech.js
@@ -1,6 +1,17 @@
 // they because they *have*
 // followd 
+
+class PronounGroup {
+  constructor(I, me, my, myself) {
+    this.I = I;
+    this.me = me;
+    this.my = my;
+    this.myself = myself;
+  }
+}
 const singular_pronouns = [
-  ["I", "she", "he","it", "they", 
-  "myself", "herself", "himself", "yourself", "itself",
-]
+  new PronounGroup("I", "me", "my", "myself"),
+  new PronounGroup("he", "him", "his", "himself"),
+  new PronounGroup("she", "her", "hers", "herself"),
+  new PronounGroup("they", "them", "their", "themselves")
+  ]
