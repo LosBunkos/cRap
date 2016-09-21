cca96541cf689144e8dbd7eaadd8486ee5b7ffe2
Sashko <Sashko>
2016-09-21 22:26:46 +0300

    adding functions to the services to send/get text

--- javascript/controller.js
+++ javascript/controller.js
@@ -1,6 +1,6 @@
 app.controller('crapCtrl', function($scope, crapService){
 
    $scope.sendRap = function(){
-    console.log($scope.getText)
+    crapService.sendRap($scope.getText)
   }
 })
--- javascript/services.js
+++ javascript/services.js
@@ -1,3 +1,16 @@
-app.service('crapService', function(){
+app.service('crapService', ['$http', function($http){
 
-})
+  var texts = {
+    text: [],
+
+    sendRap(text){
+      console.log("this is the text", text)
+      $http.post('https://3c4a602e.ngrok.io/getRap', {text:text}).success(function(data){
+        console.log(data)
+      })
+    }
+  }
+
+  return texts;
+
+}])
