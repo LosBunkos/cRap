28e85907c1be5874597198bf3c6d7bcc8d9268c7
LosBunkos <nadavvadan@gmail.com>
2016-09-21 21:39:07 +0300

    now generates 4-line raps with a post route

--- parts_of_speech.js
+++ parts_of_speech.js
@@ -6,7 +6,19 @@ const toSimple = require('verbutils')();
 const toPast = require('tensify');
 var ext = require('./word-extractor');
 
-// const extractor = require('./word-extractor');
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var app = express();
+
+app.use(express.static('javascript'));
+app.use(express.static('node_modules'));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var port = process.env.PORT || '4000';
+
 class Word {
   constructor(type) {
     this.type = type;
@@ -179,11 +191,15 @@ let adjs = [];
 
 // test
 ////////////
-let str = "If you have an author's name, but can't remember the title of the particular story\
- you are looking for, click on the above button to navigate to a page where you will be confronted\
-  by a list of the authors whose works appear here.";
-str = ext.init(str);
-var Words;
+let str = "In the beginning I humoured him, yes, that I can admit, but then a\
+ little voice in the back of my head began to whisper 'what if?' And that's when\
+  I started to over think the whole thing. I would have the opportunity to see him\
+   again. Maybe even follow and find him. Just go to the day before he left. \
+   That's all I would need. One last, long lingering look at his beautiful face so I \
+   could burn the image into my mind for good. I could even say goodbye. Hell, if I went \
+   back a week further I could stop the whole birthday incident. It almost felt unhealthy \
+   to even think these thoughts, but that didn't stop me. In fact I was beginning to feel \
+   something close to excitement. Like I was really going to get to see him again.";
 
 // ext.getWordsAndRhymes(str, (werds)=> {
 //   setTimeout(function(){
@@ -193,17 +209,46 @@ var Words;
 // })
 
 
-var genSentence = (words)=> {
+var genSentence = (words, callback)=> {
   let nouns = words.nouns.concat(words.names);
   let adjs = words.adjectives;
   let verbs = words.verbs;
+  let all = '';
   // console.log(words.verbs)
+  console.time("Got rhymes")
   nouns[0].getRhymes(()=>{
-      console.log(simpleSentence(PronounsList, [nouns[0]], verbs, adjs));
-      console.log(simpleSentence(PronounsList, nouns[0].rhymes, verbs, adjs));
+    nouns[1].getRhymes(()=> {
+
+      let sen1 = simpleSentence(PronounsList, [nouns[0]], verbs, adjs);
+      let sen2 = simpleSentence(PronounsList, [nouns[1]], verbs, adjs);
+      let sen3 = simpleSentence(PronounsList, nouns[0].rhymes, verbs, adjs);
+      let sen4 = simpleSentence(PronounsList, nouns[1].rhymes, verbs, adjs);
+      let sens = sen1 + '\n' + sen2 + '\n' + sen3 + '\n' + sen4;
+      callback(sens);
+      console.timeEnd("Got rhymes");
+
     })
+  })
 }
-ext.extractForShortSentances(str, (werds) => {
-  setTimeout(()=>{genSentence(werds)}, 100)
+
+app.post('/postRap', (req, res, next) => {
+  if (false) {//!req.body || !req.body.text) {
+    console.log('got a req with no req.body');
+    next(new Error('f u no req.body'));
+  } else {
+    console.log("Got text:", req.body.text)
+    let str = ext.init(req.body.text);
+    ext.getWordsAndRhymes(str, (werds) => {
+      setTimeout(()=> {
+        genSentence(werds, (sentences)=> {
+          res.json({sens: sentences});
+          })
+        }, 100);
+    })
+  }
+
+
 })
 
+console.log('Running server on http://localhost:' + port);
+app.listen(port);
