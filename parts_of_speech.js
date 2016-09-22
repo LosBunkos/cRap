d07eb871df19e908e8edf7a86bad72bce235dab2
LosBunkos <nadavvadan@gmail.com>
2016-09-22 09:27:49 +0300

    working routes. added morgan to log requests

--- parts_of_speech.js
+++ parts_of_speech.js
@@ -8,6 +8,7 @@ var ext = require('./word-extractor');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+var morgan = require('morgan')
 
 var app = express();
 
@@ -17,7 +18,15 @@ app.use(express.static('node_modules'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+// log requests
+app.use(morgan('combined'))
+
 var port = process.env.PORT || '4000';
+
+app.use(function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  next();
+});
 
 class Word {
   constructor(type) {
@@ -191,15 +200,16 @@ let adjs = [];
 
 // test
 ////////////
-let str = "In the beginning I humoured him, yes, that I can admit, but then a\
- little voice in the back of my head began to whisper 'what if?' And that's when\
-  I started to over think the whole thing. I would have the opportunity to see him\
-   again. Maybe even follow and find him. Just go to the day before he left. \
-   That's all I would need. One last, long lingering look at his beautiful face so I \
-   could burn the image into my mind for good. I could even say goodbye. Hell, if I went \
-   back a week further I could stop the whole birthday incident. It almost felt unhealthy \
-   to even think these thoughts, but that didn't stop me. In fact I was beginning to feel \
-   something close to excitement. Like I was really going to get to see him again.";
+// let str = "In the beginning I humoured him, yes, that I can admit, but then a\
+//  little voice in the back of my head began to whisper 'what if?' And that's when\
+//   I started to over think the whole thing. I would have the opportunity to see him\
+//    again. Maybe even follow and find him. Just go to the day before he left. \
+//    That's all I would need. One last, long lingering look at his beautiful face so I \
+//    could burn the image into my mind for good. I could even say goodbye. Hell, if I went \
+//    back a week further I could stop the whole birthday incident. It almost felt unhealthy \
+//    to even think these thoughts, but that didn't stop me. In fact I was beginning to feel \
+//    something close to excitement. Like I was really going to get to see him again.";
+// str = ext.init(str);
 
 // ext.getWordsAndRhymes(str, (werds)=> {
 //   setTimeout(function(){
@@ -210,42 +220,68 @@ let str = "In the beginning I humoured him, yes, that I can admit, but then a\
 
 
 var genSentence = (words, callback)=> {
-  let nouns = words.nouns.concat(words.names);
+  let nouns = words.nouns;
   let adjs = words.adjectives;
   let verbs = words.verbs;
   let all = '';
   // console.log(words.verbs)
   console.time("Got rhymes")
+  let start = new Date().getTime();
+
   nouns[0].getRhymes(()=>{
     nouns[1].getRhymes(()=> {
 
       let sen1 = simpleSentence(PronounsList, [nouns[0]], verbs, adjs);
       let sen2 = simpleSentence(PronounsList, [nouns[1]], verbs, adjs);
+      console.log('\n\t\t', nouns[0].rhymes, '\n');
       let sen3 = simpleSentence(PronounsList, nouns[0].rhymes, verbs, adjs);
       let sen4 = simpleSentence(PronounsList, nouns[1].rhymes, verbs, adjs);
       let sens = sen1 + '\n' + sen2 + '\n' + sen3 + '\n' + sen4;
-      callback(sens);
+      let end = new Date().getTime();
+      let time = end - start;
       console.timeEnd("Got rhymes");
+      callback({sentences: [sen1, sen2, sen3, sen4], took: time + 'ms'});
 
     })
   })
 }
 
-app.post('/postRap', (req, res, next) => {
-  if (false) {//!req.body || !req.body.text) {
+app.get('/getRap', (req, res, next) => {
+  if (false){//!req.body || !req.body.text) {
     console.log('got a req with no req.body');
     next(new Error('f u no req.body'));
-  } else {
-    console.log("Got text:", req.body.text)
-    let str = ext.init(req.body.text);
+  }
+
+    // console.log("Got text:", req.body.text)
+    // let str2 = ext.init(req.body.text);
+    console.log("\nTokenized to:", str)
     ext.getWordsAndRhymes(str, (werds) => {
       setTimeout(()=> {
         genSentence(werds, (sentences)=> {
-          res.json({sens: sentences});
+          res.json(sentences);
           })
         }, 100);
     })
+})
+
+app.post('/getRap', (req, res, next) => {
+  if (!req.body || !req.body.text) {
+    console.log('got a req with no req.body');
+    next('f u no req.body');
   }
+    let str = req.body.text;
+    console.log("\nGot text:\n\t", str);
+    let tokens = ext.init(req.body.text);
+    console.log("\nTokenized to:\n\t", tokens);
+    ext.getWordsAndRhymes(tokens, (werds) => {
+      setTimeout(()=> {
+        genSentence(werds, (sentences)=> {
+          sentences.tokens = tokens.split(' ');
+          sentences.original = str;
+          res.json(sentences);
+          })
+        }, 100);
+    })
 
 
 })
