fba8c986d8e85c3d8683541fbecbfeea146a7228
st34k <lexie.k@gmail.com>
2016-09-28 15:34:35 +0300

    trying to add a share button to the rap

--- public/javascript/controller.js
+++ public/javascript/controller.js
@@ -4,7 +4,6 @@ app.controller('crapCtrl', function($scope, crapService){
   $scope.loading = false;
 
   $scope.sendRap = function(){
-    console.log("gettext yo", $scope.getText)
     $scope.loading = true;
     crapService.sendRap($scope.getText, $scope.loading).then(function(){
       $scope.texts = crapService.text;
@@ -13,8 +12,10 @@ app.controller('crapCtrl', function($scope, crapService){
     });
   }
 
+  $scope.collectSentence = function() {
+    let sentences = $scope.texts
+    console.log(sentences)
+  }
+
   
-
-
-  setTimeout(function(){console.log("yo", $scope.texts)},5000)
 })
