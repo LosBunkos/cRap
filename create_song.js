69593b96a7b386ae5e4d33fe83c509e7038d61c2
LosBunkos <nadavvadan@gmail.com>
2016-09-28 17:33:28 +0300

    forgot to add a file

---
+++ create_song.js
@@ -0,0 +1,14 @@
+const actions = require('./sentence_actions');
+const POS = require('./parts_of_speech');
+const we = require('./extractor');
+const cs = require('create_sentence');
+
+class Crap {
+  constructor(...sens) {
+    this.sens = sens;
+  }
+
+  make(Words) {
+    
+  }
+}
